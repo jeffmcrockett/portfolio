e3201caf9989a8e2051bdd5a2720a4aabb60c970
jeffmcrockett <jeffmcrockett@gmail.com>
2020-11-08 22:02:23 -0600

    update to js file with scroll effect still now working on visibility

---
+++ script.js
@@ -0,0 +1,68 @@
+
+	$.fn.offScreen = function(distance){
+		
+	    var $t				= $(this),
+	    	$w				= $(window),
+	    	viewTop			= $w.scrollTop(),
+	    	viewBottom		= viewTop + $w.height(),
+	    	_top			= $t.offset().top - distance,
+	    	_bottom		= $t.offset().top + $t.height() + distance;
+   
+   return {
+     top: _bottom <= viewTop,
+     bottom: _top >= viewBottom
+   }
+	
+};
+    
+
+
+var win = $(window);
+
+var allMods = $(".module");
+
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (!el.offScreen(20).bottom) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.on("scroll resize",function(event) 
+{
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (!el.offScreen(150).top && !el.offScreen(20).bottom) 
+    {
+      el.removeClass("already-visible off-screen-top off-screen-bottom"); 
+      el.addClass("come-in"); 
+    } 
+    else
+    {
+    
+        if(el.offScreen(150).top)
+        {
+            el.addClass("off-screen-top"); 
+        }
+        else
+        {
+            el.addClass("off-screen-bottom"); 
+        }
+    }
+  });allMods.each()
+  
+});win.scroll()
+
+win.trigger("scroll");
+
+allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("already-visible");
+    }
+    else {
+    /*  Like this!  */
+    el.css('visibility', 'hidden');
+    }
+  });
