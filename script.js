6d1ab57cceb3539f9630407629a045fcb5afd4b8
jeffmcrockett <jeffmcrockett@gmail.com>
2020-11-30 00:33:13 -0600

    adding break images

--- script.js
+++ script.js
@@ -1,68 +1,100 @@
+// $(document).ready(function(){
 
-	$.fn.offScreen = function(distance){
+// function isElementVisible($elementToBeChecked)
+// {
+//     var TopView = $(window).scrollTop();
+//     var BotView = TopView + $(window).height();
+//     var TopElement = $elementToBeChecked.offset().top;
+//     var BotElement = TopElement + $elementToBeChecked.height();
+//     return ((BotElement <= BotView) && (TopElement >= TopView));
+// }
+
+// $(window).scroll(function () {
+//     $( ".bar" ).each(function() {
+//         $this = $(this);
+//         isOnView = isElementVisible($(this));
+//         if(isOnView && !$(this).hasClass('come-in')){
+//             $(this).addClass('come-in');
+//             startAnimation($(this));
+//         }
+//     });
+// });
+
+// function startAnimation($this) {
+//   $this.animate({
+//     width: "100%"
+//   }, 3000, function() {
+//     // Animation complete.
+//   });
+// }
+
+// });
+
+
+
+
+
+
+// 	$.fn.offScreen = function(distance){
 		
-	    var $t				= $(this),
-	    	$w				= $(window),
-	    	viewTop			= $w.scrollTop(),
-	    	viewBottom		= viewTop + $w.height(),
-	    	_top			= $t.offset().top - distance,
-	    	_bottom		= $t.offset().top + $t.height() + distance;
+// 	    var $t				= $(this),
+// 	    	$w				= $(window),
+// 	    	viewTop			= $w.scrollTop(),
+// 	    	viewBottom		= viewTop + $w.height(),
+// 	    	_top			= $t.offset().top - distance,
+// 	    	_bottom		= $t.offset().top + $t.height() + distance;
    
-   return {
-     top: _bottom <= viewTop,
-     bottom: _top >= viewBottom
-   }
+//    return {
+//      top: _bottom <= viewTop,
+//      bottom: _top >= viewBottom
+//    }
 	
-};
+// };
     
 
 
-var win = $(window);
+// var win = $(window);
 
-var allMods = $(".module");
+// var allMods = $(".module");
 
-allMods.each(function(i, el) {
-  var el = $(el);
-  if (!el.offScreen(20).bottom) {
-    el.addClass("already-visible"); 
-  } 
-});
+// allMods.each(function(i, el) {
+//   var el = $(el);
+//   if (!el.offScreen(20).bottom) {
+//     el.addClass("already-visible"); 
+//   } 
+// });
 
-win.on("scroll resize",function(event) 
-{
-  
-  allMods.each(function(i, el) {
-    var el = $(el);
-    if (!el.offScreen(150).top && !el.offScreen(20).bottom) 
-    {
-      el.removeClass("already-visible off-screen-top off-screen-bottom"); 
-      el.addClass("come-in"); 
-    } 
-    else
-    {
-    
-        if(el.offScreen(150).top)
-        {
-            el.addClass("off-screen-top"); 
-        }
-        else
-        {
-            el.addClass("off-screen-bottom"); 
-        }
-    }
-  });allMods.each()
-  
-});win.scroll()
+// win.on("scroll resize",function(event) 
+// {
+//   allMods.each(function(i, el) {
+//     var el = $(el);
+//     if (!el.offScreen(150).top && !el.offScreen(20).bottom) 
+//     {
+//       el.removeClass("already-visible off-screen-top off-screen-bottom"); 
+//       el.addClass("come-in"); 
+//     } 
+//     else
+//     {
+//         if(el.offScreen(150).top)
+//         {
+//             el.addClass("off-screen-top"); 
+//         }
+//         else
+//         {
+//             el.addClass("off-screen-bottom"); 
+//         }
+//     }
+//   });allMods.each()
+// });win.scroll()
+// win.trigger("scroll");
 
-win.trigger("scroll");
-
-allMods.each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass("already-visible");
-    }
-    else {
-    /*  Like this!  */
-    el.css('visibility', 'hidden');
-    }
-  });
+// allMods.each(function(i, el) {
+//     var el = $(el);
+//     if (el.visible(true)) {
+//       el.addClass("already-visible");
+//     }
+//     else {
+//     /*  Like this!  */
+//     el.css('visibility', 'hidden');
+//     }
+//   });
